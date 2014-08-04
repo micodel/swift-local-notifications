fcb79d14bd4935f977542cbce762f0f8c3ee06e3
Michael Hamel <hamel.michael.p@gmail.com>
2014-08-03 20:27:39 -0500

    Added project skeleton.

---
+++ swift local notifications/swift local notifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift local notifications
+//
+//  Created by Michael Hamel on 8/3/14.
+//  Copyright (c) 2014 Michael Hamel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
